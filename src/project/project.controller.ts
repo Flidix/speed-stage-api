daac318f6bc936890a0bcdb37e9b98e28ae844c6
Flidix <marko.lazarchuk@gmail.com>
2024-06-12 22:49:43 +0300

    feat: move from cookies to localStorage

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,8 +1,8 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ProjectService } from './project.service';
-import { JwtAuthGuard } from './guards/auth.guard';
 import { CurrentProject } from './decorators/current-project.decorator';
 import { CheckAuthDto } from './dto/check-auth.dto';
+import { HeadersJwtAuthGuard } from './guards/headers.auth.guard';
 
 @Controller('project')
 export class ProjectController {
@@ -18,7 +18,7 @@ export class ProjectController {
     return this.projectService.auth(dto.projectId, dto.token);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(HeadersJwtAuthGuard)
   @Get(':projectId')
   async getProject(
     @CurrentProject('projectId') projectIdFromToken: string,
